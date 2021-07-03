60b8faab5e87b90adc480b79b59c6ac708e18eef
Drunken72 <arushasp4710@gmail.com>
2021-07-04 00:04:02 +0545

    TRYING TO FIRST COMMIT

---
+++ Java/firstjavascript.js
@@ -0,0 +1,8 @@
+
+function press(){
+    console.log("5,4,3,2,1,boom!!");
+    alert("Countdown initiated");
+    document.write("A secret code")
+}
+// document.write("Nothing in here")
+document.getElementById("para 1").innerHTML = "Paragraph from javaS"
